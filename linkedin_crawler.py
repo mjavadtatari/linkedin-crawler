c46ebda77e1fbb9f6cdd46f3c26960c78a60a761
Mohammad Javad Tatari <mjavadtatari98@gmail.com>
2022-03-03 22:35:07 +0330

    change logger configuration and added SQLite DB functions

--- linkedin_crawler.py
+++ linkedin_crawler.py
@@ -1,32 +1,102 @@
 import logging
+import sqlite3
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from sqlite3 import Error
 
 # Constants Variables
-LOG_FILE = 'app.log'
+LOG_FILE_PATH = 'linkedin.log'
 LOG_FORMAT = '[%(asctime)s] %(levelname)-10s %(name)-14s %(message)s'
-# HOMEPAGE_URL = 'https://www.linkedin.com'
-# LOGIN_URL = 'https://www.linkedin.com/login'
-# TARGET_URL=''
+SQLITE_PATH = 'linkedin.sqlite'
+LINKEDIN_EMAIL = ''
+LINKEDIN_PASSWORD = ''
+LINKEDIN_LOGIN_URL = 'https://www.linkedin.com/login'
+LINKEDIN_CONNECTIONS_URL='https://www.linkedin.com/mynetwork/invite-connect/connections/'
 
 
-# Create Logger
-logger = logging.getLogger(__name__)
+# SQLite3 Queries
+create_linkedin_table = """
+CREATE TABLE IF NOT EXISTS linkedin (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    full_name TEXT NOT NULL,
+    headline TEXT,
+    education TEXT,
+    experience TEXT,
+    location TEXT,
+    about TEXT
+);
+"""
+insert_data_linkedin_table = """
+INSERT INTO
+    linkedin (full_name, headline, education, experience, location, about)
+VALUES
+    ()
+"""
 
-# Create Handler
-file_handler = logging.FileHandler(LOG_FILE, mode='w')
-file_handler.setLevel(logging.INFO)
 
-# Create Formatter and set it to Handler
-file_format = logging.Formatter(LOG_FORMAT)
-file_handler.setFormatter(file_format)
+def create_logger(path, log_format):
+    """
+    Create and configuring the logger
+    """
 
-# Add Handler to the Logger
-logger.addHandler(file_handler)
+    logger = logging.getLogger(__name__)
+    logger.setLevel('INFO')
+
+    file_handler = logging.FileHandler(path, mode='w')
+
+    file_format = logging.Formatter(log_format)
+    file_handler.setFormatter(file_format)
+
+    logger.addHandler(file_handler)
+
+    return logger
 
 
-logger.warning('This is a warning message')
-logger.error('This is an error message')
-logger.critical('This is a critical message')
-logger.info('This is an info message')
+def create_connection(path, logger):
+    """
+    Create a connection and returns the sqlite3 object
+    """
+
+    sqlite_db = None
+
+    try:
+        sqlite_db = sqlite3.connect(path)
+        logger.info("Connected to SQLite DB successfully")
+    except Error as e:
+        logger.error(f"SQLite Error: {e}")
+
+    return sqlite_db
+
+
+def execute_query(sqlite_db, query, logger):
+    """
+    Execute queries and log the messages
+    """
+
+    cursor = sqlite_db.cursor()
+
+    try:
+        cursor.execute(query)
+        sqlite_db.commit()
+        logger.info("Query Executed Successfully")
+    except Error as e:
+        logger.error(f"Query Execution Error: {e}")
+
+
+def insert_into_db(sqlite_db, logger):
+    """
+    Inserting Linkedin data to the DB
+    """
+
+    pass
+
+
+logger = create_logger(LOG_FILE_PATH, LOG_FORMAT)
+app_db = create_connection(SQLITE_PATH, logger)
+execute_query(app_db, create_linkedin_table, logger)
+
+# logger.warning('This is a warning message')
+# logger.error('This is an error message')
+# logger.critical('This is a critical message')
+# logger.info('This is an info message')
